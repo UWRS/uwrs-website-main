23729c01cfee70eb5818c06bcc1484d27c3cf39f
Theo Weeden <theo.weeden@gmail.com>
2022-02-27 23:57:51 +0000

    Added the strapline beneath the logo

--- uwrs-website-main/src/App.vue
+++ uwrs-website-main/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <img id="logo" src="./assets/logo/logo - colour.svg">
+    <div id="logosection">
+      <img id="logo" src="./assets/logo/logo - colour.svg">
+      <p>Hub for University of Warwick hardware hobbyists from across all disciplines.</p>
+      <p>Making robotics accessible to everyone.</p>
+    </div>
     <div id="navbar">
       <v-btn>About Us</v-btn>
       <v-btn>Projects/Events</v-btn>
@@ -30,4 +34,6 @@ export default {
 #logo{
   width: 500px;
 }
+
+
 </style>
