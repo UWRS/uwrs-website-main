6b3a84f7d082326feea74c9c9c86d0f52902c796
Theo Weeden <theo.weeden@gmail.com>
2022-02-28 00:54:01 +0000

    made the logo section into a link to the homepage

--- uwrs-website-main/src/App.vue
+++ uwrs-website-main/src/App.vue
@@ -1,16 +1,16 @@
 <template>
 <v-app><v-main>
   <div id="app">
-    <div id="logosection" class="pb-5 pt-5">
-      <img id="logo" src="./assets/logo/logo - colour.svg">
-      <p>Hub for University of Warwick hardware hobbyists from across all disciplines.</p>
-      <p>Making robotics accessible to everyone.</p>
+    <div id="logosection" onclick="window.location='/';" class="pb-5 pt-5">
+        <img id="logo" src="./assets/logo/logo - colour.svg">
+        <p>Hub for University of Warwick hardware hobbyists from across all disciplines.</p>
+        <p>Making robotics accessible to everyone.</p>
     </div>
     <div id="navbar" class="pb-5">
       <v-btn href="/">About Us</v-btn>
       <v-btn href="/">Projects/Events</v-btn>
       <v-btn href="/">For Sponsors</v-btn>
-      <v-btn href="/JoinUs">Sign Up</v-btn>
+      <v-btn href="/#/JoinUs">Sign Up</v-btn>
     </div>
     <router-view/>
   </div>
@@ -33,6 +33,6 @@ export default {
 }
 
 #logosection{
-
+  cursor: pointer;
 }
 </style>
