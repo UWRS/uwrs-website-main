79df6b69ab4e1932451b5bcf957ef73601688e3d
Theo Weeden <theo.weeden@gmail.com>
2022-02-28 00:16:25 +0000

    added a carousel for some images

--- uwrs-website-main/src/components/HelloWorld.vue
+++ uwrs-website-main/src/components/HelloWorld.vue
@@ -5,7 +5,17 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       <v-btn href="/#/JoinUs">Join Us</v-btn>
     </div>
-    <div>
+
+    <v-carousel>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+    <!--<div>
       <h1>What We Do</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
     </div>
@@ -20,7 +30,7 @@
     <div>
       <h1>Our Newsletter</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    </div>
+    </div>-->
   </div>
 </template>
 
@@ -29,6 +39,17 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      items: [
+        {
+          src: require('../assets/photos/cat robot.jpg'),
+        },
+        {
+          src: require('../assets/photos/CAD.jpg'),
+        },
+        {
+          src: require('../assets/photos/robot.jpg'),
+        },
+      ]
     }
   }
 }
