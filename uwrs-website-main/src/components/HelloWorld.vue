91104e044e33823038d639a1c27621bc2822ca94
Theo Weeden <theo.weeden@gmail.com>
2022-02-28 00:53:26 +0000

    commented out the mockup stuff

--- uwrs-website-main/src/components/HelloWorld.vue
+++ uwrs-website-main/src/components/HelloWorld.vue
@@ -10,13 +10,14 @@
       ></v-carousel-item>
     </v-carousel>
 
+    <!--
+    This is the a layout from the original paper mock up  
     <div>
       <h1>Who We Are</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       <v-btn href="/#/JoinUs">Join Us</v-btn>
     </div>
-
-    <!--<div>
+    <div>
       <h1>What We Do</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
     </div>
