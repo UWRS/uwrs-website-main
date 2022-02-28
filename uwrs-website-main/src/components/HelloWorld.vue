aa2a7dc04b840e2785756820f85247b437561322
Theo Weeden <theo.weeden@gmail.com>
2022-02-28 00:00:51 +0000

    added a link to the join us page

--- uwrs-website-main/src/components/HelloWorld.vue
+++ uwrs-website-main/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <div>
       <h1>Who We Are</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-      <v-btn>Join Us</v-btn>
+      <v-btn href="/#/JoinUs">Join Us</v-btn>
     </div>
     <div>
       <h1>What We Do</h1>
