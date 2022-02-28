baeb86ce43e597df59ddbd919a5016ba45a75e24
Theo Weeden <theo.weeden@gmail.com>
2022-02-28 01:10:22 +0000

    changed the style of the carousel

--- uwrs-website-main/src/components/Home.vue
+++ uwrs-website-main/src/components/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="mainbody">
-    <v-carousel>
+    <v-carousel 
+      cycle
+      height="800"
+      interval="10000"
+      hide-delimiter-background
+      show-arrows-on-hover>
       <v-carousel-item
         v-for="(item,i) in items"
         :key="i"
         :src="item.src"
-        reverse-transition="fade-transition"
-        transition="fade-transition"
       ></v-carousel-item>
     </v-carousel>
 
